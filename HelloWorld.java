9f72f80d8e0bdd6861fd150f9a555292d769ba7e
AnithaPegadaraju <93178774+AnithaPegadaraju@users.noreply.github.com>
2021-12-13 12:12:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello World!, WELCOME TO LEENA IT Services Pvt Ltd"); 
+        system.out.println("I am adding onemor line to existing program");
         
         int n1 = 225, n2 = 115, sum;  
         sum = n1 + n2;  
